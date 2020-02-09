e4b6d9976e89c4b5cb1d109c2fa0686a343620bb
Shota Nemoto <srn24@case.edu>
2020-02-08 21:45:39 -0500

    added command pulling

--- cmd_parser/cmd_parser.py
+++ cmd_parser/cmd_parser.py
@@ -2,18 +2,21 @@ import requests
 import time
 
 URL = 'https://botcontroller-267620.appspot.com/commands/'
-UUID = "something"
+UUID = "2adb80d2-5e2d-4c8f-9558-2aeb8f307077"
 
 if __name__=="__main__":
+    requests.delete(URL + UUID)
     sequence_counter = 0
     while True:
-        cmd_str = requests.get(URL + UUID)
-        cmds = [cmd.split(",") for cmd in cmd_str.split("\n")]
-        
-        last_seq_num = int(cmds[-1][0])
-        if last_seq_num > sequence_counter:
-            new_cmds = cmds[sequence_counter - last_seq_num : ]
-            sequence_counter = last_seq_num
-
+        cmd_response = requests.get(URL + UUID)
+        if cmd_response.ok:
+            cmd_str = cmd_response.text
+            cmds = [cmd.split(",") for cmd in cmd_str.split("\n")][:-1]
+            
+            last_seq_num = int(cmds[-1][0])
+            if last_seq_num > sequence_counter:
+                new_cmds = cmds[sequence_counter - last_seq_num : ]
+                print(new_cmds)
+                sequence_counter = last_seq_num
 
         time.sleep(.05)
